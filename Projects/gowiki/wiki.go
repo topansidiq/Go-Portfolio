6d6c1120243bf8526ec7aa962b6837f6310ecc61
topansidiq <topansidiq@gmail.com>
2025-02-18 21:21:59 +0700

    Menyelesaikan tahap kedua aplikasi web

--- Projects/gowiki/wiki.go
+++ Projects/gowiki/wiki.go
@@ -5,23 +5,23 @@ import (
 	"log"
 	"net/http"
 	"os"
+	"path/filepath"
 	"regexp"
 )
 
 // Page Service
-
 type Page struct {
 	Title string
 	Body  []byte
 }
 
 func (p *Page) save() error {
-	filename := p.Title + ".txt"
+	filename := filepath.Join("data", p.Title+".txt")
 	return os.WriteFile(filename, p.Body, 0600)
 }
 
 func loadPage(title string) (*Page, error) {
-	filename := title + ".txt"
+	filename := filepath.Join("data", title+".txt")
 	body, err := os.ReadFile(filename)
 	if err != nil {
 		return nil, err
@@ -29,7 +29,26 @@ func loadPage(title string) (*Page, error) {
 	return &Page{Title: title, Body: body}, nil
 }
 
+// Caching
+var templates *template.Template
+
+// Menambahkan fungsi konversi []byte ke string dalam template
+func init() {
+	// Menambahkan fungsi ke dalam template.FuncMap terlebih dahulu
+	templates = template.New("").Funcs(template.FuncMap{
+		"bytesToString": func(b []byte) string {
+			return string(b) // Konversi []byte ke string
+		},
+	})
+
+	// Parsing template setelah fungsi ditambahkan
+	templates = template.Must(templates.ParseGlob("tmpl/*.html"))
+}
+
 // Handler
+func rootHandler(w http.ResponseWriter, r *http.Request) {
+	http.Redirect(w, r, "/view/FrontPage", http.StatusFound)
+}
 
 func makeHandler(fn func(http.ResponseWriter, *http.Request, string)) http.HandlerFunc {
 	return func(w http.ResponseWriter, r *http.Request) {
@@ -48,6 +67,7 @@ func viewHandler(w http.ResponseWriter, r *http.Request, title string) {
 		http.Redirect(w, r, "/edit/"+title, http.StatusFound)
 		return
 	}
+	p.Body = convertLinks(p.Body)
 	renderTemplate(w, "view", p)
 }
 
@@ -77,17 +97,28 @@ func renderTemplate(w http.ResponseWriter, tmpl string, p *Page) {
 	}
 }
 
-// Caching
+// Inter-page linking
+var linkPattern = regexp.MustCompile(`\[(.+?)\]`)
 
-var templates = template.Must(template.ParseFiles("edit.html", "view.html"))
+func convertLinks(text []byte) []byte {
+	return linkPattern.ReplaceAllFunc(text, func(match []byte) []byte {
+		pageName := string(match[1 : len(match)-1])
+		pageURL := "/view/" + regexp.MustCompile(`\s+`).ReplaceAllString(pageName, "%20")
+		return []byte(`<a href="` + pageURL + `">` + template.HTMLEscapeString(pageName) + `</a>`)
+	})
+}
 
 // Validation
-
-var validPath = regexp.MustCompile("^/(edit|save|view)/([a-zA-Z0-9]+)$")
+var validPath = regexp.MustCompile("^/(edit|save|view)/([a-zA-Z0-9_-]+)$")
 
 func main() {
+	http.HandleFunc("/", rootHandler)
 	http.HandleFunc("/view/", makeHandler(viewHandler))
 	http.HandleFunc("/edit/", makeHandler(editHandler))
 	http.HandleFunc("/save/", makeHandler(saveHandler))
+
+	http.Handle("/static/", http.StripPrefix("/static/", http.FileServer(http.Dir("static"))))
+
+	log.Println("Server berjalan di http://localhost:8080")
 	log.Fatal(http.ListenAndServe(":8080", nil))
 }
